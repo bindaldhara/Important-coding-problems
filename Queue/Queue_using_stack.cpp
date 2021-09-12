4e80c77758971c54b631dedb507e56a13a5452db
bindaldhara <84957152+bindaldhara@users.noreply.github.com>
2021-09-12 12:08:15 +0530

    Create Queue_using_stack.cpp

---
+++ Queue/Queue_using_stack.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+class Queue{
+    stack<int>s1;
+    stack<int>s2;
+    public:
+    void push(int x){
+        s1.push(x);
+    }
+     int pop(){
+         if(s1.empty()&&s2.empty()){
+         cout<<"queue is empty";
+         return -1;
+         }
+         if(s2.empty()){
+             while(!s1.empty()){
+                 s2.push(s1.top());
+                 s1.pop();
+             }
+         }
+         int topval=s2.top();
+         s2.pop();
+         return topval;
+     }
+     bool empty(){
+         if(s1.empty()&& s2.empty())
+         return true;
+         return false;
+     }
+};
+int main(){
+    Queue q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    cout<<q.pop();
+    return 0;
+}
