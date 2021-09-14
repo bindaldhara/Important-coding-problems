750438a0b2c7b091058f890794af8ca2ff0e8d8f
bindaldhara <84957152+bindaldhara@users.noreply.github.com>
2021-09-14 14:36:03 +0530

    Create Tree_from_inorder_and_preorder.cpp

---
+++ Trees/Tree_from_inorder_and_preorder.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+     Node(int val){
+        int data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+int search(int inorder[],int start,int end,int curr){
+  for(int i=start;i<=end;i++){
+      if(inorder[i]==curr)
+      return i;
+  }
+  return -1;
+}
+
+Node* build(int preorder[],int inorder[],int start,int end){
+    static int idx=0;
+    if(start>end)
+    return NULL;
+    int curr=preorder[idx];
+    idx++;
+    Node* node=new Node(curr);
+    if(start==end)
+    return node;
+    int pos=search(inorder,start,end,curr);
+    node->left=build(preorder,inorder,start,pos-1);
+    node->right=build(preorder,inorder,pos+1,end);
+
+    return node;
+}
+
+void inorderprint(Node* root){
+    if(root==NULL)
+    return;
+    inorderprint(root->left);
+    std::cout<<root->data<<" ";
+    inorderprint(root->right);
+}
+
+int main(){
+int preorder[]={1,2,4,3,5};
+int inorder[]={4,2,1,5,3};
+ Node* root=build(preorder,inorder,0,4);
+inorderprint(root);
+return 0;
+}
