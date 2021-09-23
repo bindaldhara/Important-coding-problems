09378283264e3771c426ebe6cfe3ea71e769e776
bindaldhara <84957152+bindaldhara@users.noreply.github.com>
2021-09-23 15:35:10 +0530

    Create kth_largest_element.cpp

---
+++ Heap/kth_largest_element.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+int  kth(int arr[],int n,int k){
+priority_queue<int, vector<int>, greater<int>> pq;
+        for (int i = 0; i < n; i++) 
+        {
+            //if size of priority queue becomes equal to k and top 
+            //element is smaller than arr[i], we pop the front 
+            //element and push arr[i] in priority queue.
+            if (pq.size() == k && pq.top() < arr[i]) 
+            { 
+                pq.pop();
+                pq.push(arr[i]);
+            } 
+            //else we just push arr[i] in priority queue.
+            else if (pq.size() < k)
+                pq.push(arr[i]);
+        }
+        //returning the top element of priority queue.
+        return pq.top();
+}
+int main()
+{
+    int arr[] = {4,3,7,6,5};
+    int k = 5;
+    int n=5;
+    cout<<"Kth largest element is:"<<kth(arr,n,k);
+    return 0;
+}
