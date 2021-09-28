f6f70755ce1920adace493a142bed6d1bf3ab92e
bindaldhara <84957152+bindaldhara@users.noreply.github.com>
2021-09-28 12:33:49 +0530

    Create Median_of_stream.cpp

---
+++ Heap/Median_of_stream.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+void printMedians(double arr[], int n) 
+{ 
+    // max heap to store the smaller half elements 
+    priority_queue<double> s; 
+    // min heap to store the greater half elements 
+    priority_queue<double,vector<double>,greater<double> > g; 
+    double med = arr[0]; 
+    s.push(arr[0]); 
+    cout << med << endl; 
+    for (int i=1; i < n; i++) { 
+        double x = arr[i]; 
+        if (s.size() > g.size()) 
+        { 
+            if (x < med) 
+            { 
+                g.push(s.top()); 
+                s.pop(); 
+                s.push(x); 
+            } 
+            else
+                g.push(x); 
+  
+            med = (s.top() + g.top())/2.0; 
+        } 
+        else if (s.size()==g.size()) 
+        { 
+            if (x < med) 
+            { 
+                s.push(x); 
+                med = (double)s.top(); 
+            } 
+            else
+            { 
+                g.push(x); 
+                med = (double)g.top(); 
+            } 
+        } 
+        else
+        { 
+            if (x > med) 
+            { 
+                s.push(g.top()); 
+                g.pop(); 
+                g.push(x); 
+            } 
+            else
+                s.push(x); 
+  
+            med = (s.top() + g.top())/2.0; 
+        } 
+  
+        cout << med << endl; 
+    } 
+} 
+  
+// Driver program to test above functions 
+int main() 
+{ 
+    // stream of integers 
+    double arr[] = {5, 15, 1,3}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    printMedians(arr, n); 
+    return 0; 
+}
