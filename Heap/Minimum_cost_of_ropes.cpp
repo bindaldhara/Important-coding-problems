7a24c308e8851a9ac9842a73a4ee2984db9869b8
bindaldhara <84957152+bindaldhara@users.noreply.github.com>
2021-09-29 11:34:00 +0530

    Create Minimum_cost_of_ropes.cpp

---
+++ Heap/Minimum_cost_of_ropes.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <queue>
+using namespace std;
+ 
+int findMinCost(vector<int> &arr){
+   //min heap
+    priority_queue<int, vector<int>, greater<int>> pq(arr.begin(), arr.end());
+
+    int res = 0;
+    while (pq.size() > 1)
+    {
+        int x = pq.top();
+        pq.pop();
+ 
+        int y = pq.top();
+        pq.pop();
+
+        int sum = x + y;
+        pq.push(sum);
+        res += sum;
+    }
+    return res;
+}
+
+int main(){
+    vector<int> arr = { 4,3,2,6 };
+    cout << "The minimum cost is " << findMinCost(arr);
+    return 0;
+}
